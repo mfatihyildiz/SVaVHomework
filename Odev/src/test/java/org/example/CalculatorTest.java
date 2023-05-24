184432e0435d8280e3a66d26f2c392e4ad83bf8b
Mehmet Fatih Yıldız <93758135+mfatihyildiz@users.noreply.github.com>
2023-05-24 12:53:23 +0300

    Add files via upload
    Homework Uploaded

---
+++ Odev/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,37 @@
+/************
+ T.C. Sakarya University
+ SWE202 Software Verification and Validation Homework 1
+ Mehmet Fatih YILDIZ
+ B201202013
+
+ https://github.com/mfatihyildiz/SVaVHomework
+ */
+
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 2, 5",
+            "10, 4, 2.5",
+            "12.5, 5, 2.5",
+            "10, 2.5, 4",
+            "12.5, 2.5, 5"
+    })
+    void testDivision(double num1, double num2, double result) {
+        assertEquals(result, Calculator.divide(num1, num2));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        assertThrows(IllegalArgumentException.class, () -> Calculator.divide(12.5, 0),
+                "IllegalArgumentException expected.");
+    }
+}

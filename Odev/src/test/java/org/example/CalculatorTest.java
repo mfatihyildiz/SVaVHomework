24e07c44dd15e8e889a92347bdf447d66fb7073c
Mehmet Fatih Yıldız <93758135+mfatihyildiz@users.noreply.github.com>
2023-05-25 09:15:36 +0300

    Test file updated

--- Odev/src/test/java/org/example/CalculatorTest.java
+++ Odev/src/test/java/org/example/CalculatorTest.java
@@ -9,6 +9,8 @@
 
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,6 +18,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 
     @ParameterizedTest
     @CsvSource({
